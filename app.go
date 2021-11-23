ca38749e857d55da2129070f093da914d0554159
Konstantin <konstantin.belov@gmail.com>
2021-11-23 15:22:45 +0300

    refactor: moving command code to dedicated func refactor: replacing fmt with log refactor: reworking crawling procedure

--- app.go
+++ app.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"log"
-
 	"net/http"
 	"net/url"
 	"os"
+	"os/signal"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/urfave/cli/v2"
 )
 
+type myURL struct {
+	url string
+	body                    []byte
+}
 // Parse & check provided URL string
 func url_parse(s string) (*url.URL, error) {
 	uri, err := url.Parse(s)
@@ -36,73 +40,134 @@ func createDstDir(p string) {
 	}
 }
 
-
-func fetch(url string, ch chan<- string) {
-	resp, err := http.Get(url)
+// Fetch one URL per time and send page body to next step
+func (u *myURL) fetch() {
+	log.Printf("Fetching %v", u.url)
+	resp, err := http.Get(u.url)
 	if err != nil {
-		ch <- fmt.Sprint(err)
+		log.Fatalln(err)
 	}
 	// Read response body and close stream
 	b, err := ioutil.ReadAll(resp.Body)
 	defer resp.Body.Close()
-
 	if err != nil {
-		ch <- fmt.Sprintf("error while reading %s: %v", url, err)
+		log.Fatalf("error while reading %s: %v", u.url, err)
 		return
 	}
+	u.body = b
+}
+
+// Parse HTML body and extrac HREFs
+func (u *myURL) getURLs() []string {
+	var hrefs []string
 	// Load HTML content from recieved bytes
-	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(b))
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(u.body))
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	doc.Find("a").Each(func(i int, s *goquery.Selection) {
-		title := s.Text()
 		link, _ := s.Attr("href")
-		fmt.Printf("Post #%d: %s - %s\n", i, title, link)
+		hrefs = append(hrefs, link)
 	})
-
-	ch <- fmt.Sprintf("Read: %7d %s", 0, url)
+	// Send extracted HREFs to next step
+	return hrefs
 }
 
 // TODO:How to deal with invalid SSL certs?
 
+// Process input arguments
+func parse_arguments(c *cli.Context) (string, string, error) {
+			if c.Args().Len() < 2 {
+		log.Printf("Please provide all required arguments: %v\n", c.App.UsageText)
+		return "", "", nil
+			}
+	base_url := c.Args().Get(0)
+
+			dst := c.Args().Get(1)
+			if dst == "" {
+		log.Printf("Please provide non empty output path")
+		return "", "", nil
+	}
+
+	_, err := url_parse(base_url)
+	if err != nil {
+		log.Fatal("Cannot parse URL, please provide correct URL")
+		return "", "", err
+	}
+
+	return base_url, dst, nil
+}
+
+// Main URL-processing function
+func crawl(url string, ch chan []string) {
+	var extracted []string
+	u := myURL{url: url}
+	u.fetch()
+	extracted = u.getURLs()
+	log.Printf("Saving data from %v", u.url)
+	ch <- extracted
+}
+
+// Validate URL string against rules
+func check_url(base_url *string, link *string) error {
+	log.Printf("LINK: %v. HTTP? %v", link, strings.HasPrefix("http", *link))
+	if !strings.HasPrefix(*base_url, *link) && !strings.HasPrefix("http", *link) {
+		*link = *base_url + *link
+	}
+	_, err := url_parse(*link)
+	return err
+	}
+func crawl_cmd(c *cli.Context) error {
+	base_url, dst, err := parse_arguments(c)
+	if err != nil {
+		panic("Error. Please check input params")
+			}
+	ch := make(chan []string, 4)
+
+	log.Printf("Base URL: %q", base_url)
+	log.Printf("Output dir: %q", dst)
+
+			createDstDir(dst)
+	// Setup Ctrl-C handler
+	signals_ch := make(chan os.Signal, 1)
+	signal.Notify(signals_ch, os.Interrupt)
+	// Send base_url for processing
+	// TODO: Looks ugly, need to find better solution
+	go func(b string) {
+		log.Printf("Sending base URL %v", b)
+		ch <- []string{b}
+	}(base_url)
+
+	for {
+		select {
+		case links := <-ch:
+			for _, link := range links {
+				err = check_url(&base_url, &link)
+				if err != nil {
+					log.Printf("Incorrect URL: %v, %v", link, err)
+					continue
+				}
+
+				go crawl(link, ch)
+			}
+		case <-signals_ch:
+			log.Println("Shutting down...")
+			return nil
+		}
+	}
+	}
+
 func main() {
+	// Setup CLI commands
 	app := &cli.App{
 		Name:      "krawler",
 		Usage:     "It takes URL string, processes it, and stores allowed content into DST directory.",
 		UsageText: "krawler <URL> <Output dir>",
-		Action: func(c *cli.Context) error {
-			if c.Args().Len() < 2 {
-				fmt.Printf("Please provide all required arguments: %v\n", c.App.UsageText)
-				return nil
-			}
-			// get URL from user
-			url_string := c.Args().Get(0)
-			// TODO: Add empty string handler
-			// get DST dir from user
-			dst := c.Args().Get(1)
-			// TODO: Add empty string handler
-			if dst == "" {
-				fmt.Printf("SSS")
-			}
-			// validate/parse URL
-			uri, err := url_parse(url_string)
-			// validate/parse DST
-			createDstDir(dst)
-			// if OKx2 -> continue, else -> return error
-			fmt.Printf("Input: %q\n", url_string)
-			fmt.Printf("URL: %q \nDST: %q\n", uri.Host, dst)
-			ch := make(chan string)
-			go fetch(uri.String(), ch)
-			fmt.Println(<-ch)
-			return err
-		},
+		Action:    crawl_cmd,
 	}
-
+	// Launch app
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
